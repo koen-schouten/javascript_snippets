0b6a3295abfa6c915b3092052a9e3d3d93c63422
Koen Schouten <schouten_koen@hotmail.com>
2018-08-16 21:07:26 +0200

    Added randomElement function

--- randomInt.js
+++ randomInt.js
@@ -8,3 +8,14 @@
 function randomInt(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+/**
+ * Picks a random element from an array.
+ * @param {array} the array
+ * 
+ * @return an element from the array
+ */
+function randomElement(array){
+    return array[randomInt(0, array.length-1)];
+}
+
