4610a2f24939901bb41de32888aa2749ec8da5b4
Koen Schouten <schouten_koen@hotmail.com>
2018-08-16 20:13:40 +0200

    Added comment to randomInt function

--- randomInt.js
+++ randomInt.js
@@ -1,3 +1,10 @@
+/**
+ * Generates a random integer between min and max.
+ * @param {number} min The lowest value
+ * @param {number} max The highest value
+ * 
+ * @return {number} a random number between min and max
+ */
 function randomInt(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
